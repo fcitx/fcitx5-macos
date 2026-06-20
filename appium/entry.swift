d6a634a31b769a8c80ad03d784a91aaff1fa0e80
Qijia Liu <liumeo@pku.edu.cn>
2026-06-19 20:51:27 -0400

    TestApp: graceful quit; debugger integration

--- appium/entry.swift
+++ appium/entry.swift
@@ -1,9 +1,21 @@
 import Fcitx
 import SwiftUI
 
+private func signalHandler(signal: Int32) {
+  Task { @MainActor in
+    NSApp.terminate(nil)
+  }
+}
+
 class TestAppDelegate: NSObject, NSApplicationDelegate {
   func applicationDidFinishLaunching(_ notification: Notification) {
+    signal(SIGINT, signalHandler)
+    signal(SIGTERM, signalHandler)
     start_fcitx_thread("")
+  }
+
+  func applicationWillTerminate(_ notification: Notification) {
+    stop_fcitx_thread()
   }
 
   func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
